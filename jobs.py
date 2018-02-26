37442d00093b52cc477663d86e1a75cd268dc932
fahmed3 <fahmed3@stuy.edu>
2018-02-26 14:06:00 -0500

    ad

--- jobs.py
+++ jobs.py
@@ -4,10 +4,12 @@ https://data.ny.gov/api/views/pxa9-czw8/rows.json?accessType=DOWNLOAD
 Imported into database by going through the lists of jobs and turning them into dictionaries
 with industry and location, then inserting that list of documents into the database.
 '''
-
+from flask import Flask, render_template, redirect, url_for
 import pymongo
 import json
 
+
+app = Flask(__name__)
 connection = pymongo.MongoClient('homer.stuy.edu')
 
 db = connection['ahmedF-parkP']
@@ -16,7 +18,6 @@ collection = db['jobs']
 def importJobs():
     jobs = []
     jobData = json.load(open("jobs.json"))['data']
-    #jobData consists of lists of lists so turning it into a list of dictionaries
     for j in jobData:
         job = {}
         job["location"] = j[9]
@@ -29,14 +30,35 @@ def importJobs():
     
 def location(l):
     location = collection.find({'location' : l})
+    retList = []
+    print type(retList)
     for i in location:
-        print i
+        retList.append(i)
+    return retList
 
-#location("New York City")
+#print location("New York City")
         
 def industry(j):
     industry = collection.find({'industry' : j})
+    retList = []
     for i in industry:
-        print i
+        retList.append(i)
+    return retList
 
-#industry("Accommodation and Food Services")
+#print industry("Accommodation and Food Services")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=["GET","POST"])
+def results():
+    if request.method == 'POST':
+        location = request.form.get('loc')
+        ind = request.form.get('ind')
+    return render_template('results.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
